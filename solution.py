d734fb4dc96059052392573305bd5fe8e58ff9de
Büşra <busraoran043@gmail.com>
2021-04-02 15:08:55 +0300

    first commit

---
+++ solution.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+import math
+
+breast_data = pd.read_csv("breast_data.csv", header=None)
+BD = (breast_data.iloc[:,:].values)
+
+breast_truth = pd.read_csv("breast_truth.csv", header=None)
+BT = (breast_truth.iloc[:].values)
+
+centroid_number = 2
+centroids = []
+
+for i in range(centroid_number):
+    centroids.append(BD[np.random.randint(1,570)])
+
+
+for i in range(10):
+    
+    cluster1 = []
+    cluster2 = []
+
+    for data in range(569):
+        a = 0
+        b = 0
+        for i in range(30):
+            a = a + (BD[data][i] - centroids[0][i])**2
+            b = b + (BD[data][i] - centroids[1][i])**2
+
+        if np.sqrt(a)<np.sqrt(b):
+            cluster1.append(BD[data])
+        else:
+            cluster2.append(BD[data])
+
+    new_centroid1 = np.mean(cluster1, axis=0)
+    new_centroid2 = np.mean(cluster2, axis=0)
+
+    centroids = []
+    centroids.append(new_centroid1)
+    centroids.append(new_centroid2)
+
+    new_cluster1 = []
+    new_cluster2 = []
+
+    predictions = []
+
+    for data in range(569):
+        a = 0
+        b = 0
+        for i in range(30):
+            a = a + (BD[data][i] - new_centroid1[i])**2
+            b = b + (BD[data][i] - new_centroid2[i])**2
+
+        if np.sqrt(a)<np.sqrt(b):
+            new_cluster1.append(BD[data])
+            predictions.append(0)
+            
+        else:
+            new_cluster2.append(BD[data])
+            predictions.append(1)
+
+    #print(cluster2)
+    #print(f"1. elaman: {BD[0]} ")
+    #print(new_centroid2)
+    print(len(new_cluster1))
+    print(len(new_cluster2))
+
+
+
+    # for i in range(569):
+    #     if BD[i] in new_cluster1:
+    #         predictions.append('0')
+    #     else:
+    #         predictions.append('1')
+    #print(BD[0])        
+    #print(predictions)
+
+    sum = 0
+    for i in range(569):
+        if BT[i] == predictions[i]:
+            sum += 1
+        
+    print(f"Accuracy: %{sum/569 * 100}")
+
+    #centroids = []
